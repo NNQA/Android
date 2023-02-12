6926757b2e6e52bb06caa9fc20156e7d5c16694f
NNQA <anhla12h@gmail.com>
2023-02-12 20:50:09 +0700

    Insert RelativeLayout

---
+++ Weeken2/RelativeLayout/app/src/main/java/com/example/firstapplication/MainActivity.java
@@ -0,0 +1,34 @@
+package com.example.firstapplication;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.MotionEvent;
+import android.view.autofill.AutofillValue;
+import android.widget.CompoundButton;
+import android.widget.Switch;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.relativelayout);
+//        TextView textView = (TextView) findViewById(R.id.text_id);
+//        Switch sw = findViewById(R.id.switch1);
+//        sw.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+//            @Override
+//            public void onCheckedChanged(CompoundButton compoundButton, boolean b) {
+//                if(b) {
+//                    textView.setText("hi em ten la quoc anh");
+//                } else {
+//                    textView.setText("");
+//                    textView.append("em code hoi bi. chicken");
+//                }
+//            }
+//        });
+
+
+    }
+}
