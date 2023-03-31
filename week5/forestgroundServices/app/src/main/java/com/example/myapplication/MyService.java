af9b43da46ff807b73eac71d7cde872342d7bd93
NNQA <anhla12h@gmail.com>
2023-03-31 14:15:50 +0700

    add new lab week 7

--- week5/forestgroundServices/app/src/main/java/com/example/myapplication/MyService.java
+++ week5/forestgroundServices/app/src/main/java/com/example/myapplication/MyService.java
@@ -6,6 +6,8 @@ import static com.example.myapplication.Application.CHANNEL_ID;
 import android.app.Notification;
 import android.app.PendingIntent;
 import android.app.Service;
+import android.content.BroadcastReceiver;
+import android.content.Context;
 import android.content.Intent;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
@@ -15,10 +17,20 @@ import android.os.IBinder;
 import android.util.Log;
 import android.widget.RemoteViews;
 
-import androidx.core.app.NotificationCompat;
+import androidx.localbroadcastmanager.content.LocalBroadcastManager;
+
+import org.jetbrains.annotations.NotNull;
 
 public class MyService extends Service {
     private MediaPlayer mediaPlayer;
+    private static final int ACTION_PAUSE = 1;
+    private static final int ACTION_RESUME = 2;
+    private static final int ACTION_CLEAR = 3;
+
+    private static final int ACTION_STRART = 4;
+    private BroadcastReceiver broadcastReceiver;
+    private boolean isPlaying;
+    private Song song1;
     public MyService() {
     }
 
@@ -34,10 +46,14 @@ public class MyService extends Service {
         if(bundle != null) {
             Song song = (Song) bundle.get("object_song");
             if(song != null) {
+                song1 = song;
                 startMusic(song);
                 sendNotification(song);
             }
         }
+
+        int actionMusic = intent.getIntExtra("actionMusic_service", 0);
+        handleActionMusic(actionMusic);
 
         return START_NOT_STICKY;
     }
@@ -47,6 +63,41 @@ public class MyService extends Service {
             mediaPlayer = MediaPlayer.create(getApplicationContext(),song.getResource());
         }
         mediaPlayer.start();
+        isPlaying = true;
+        sendActiontoActivity(ACTION_STRART);
+    }
+
+    private  void handleActionMusic(int action) {
+        switch (action) {
+            case ACTION_PAUSE:
+                pauseMusic();
+                break;
+            case ACTION_RESUME:
+                resumeMusic();
+                break;
+            case ACTION_CLEAR:
+                stopSelf();
+                sendActiontoActivity(ACTION_CLEAR);
+                break;
+        }
+    }
+
+    private void pauseMusic() {
+        if(mediaPlayer != null && isPlaying) {
+            mediaPlayer.pause();
+            isPlaying = false;
+            sendNotification(song1);
+            sendActiontoActivity(ACTION_PAUSE);
+        }
+    }
+
+    private void resumeMusic() {
+        if(mediaPlayer != null && !isPlaying) {
+            mediaPlayer.start();
+            isPlaying = true;
+            sendNotification(song1);
+            sendActiontoActivity(ACTION_RESUME);
+        }
     }
 
     private void sendNotification(Song song) {
@@ -58,7 +109,15 @@ public class MyService extends Service {
         remoteViews.setTextViewText(R.id.single, song.getSingle());
         remoteViews.setImageViewBitmap(R.id.imgsong,bitmap);
 
-        remoteViews.setImageViewResource(R.id.imgAction, R.drawable.pause);
+        remoteViews.setImageViewResource(R.id.imgPause, R.drawable.pause);
+        if(isPlaying) {
+            remoteViews.setOnClickPendingIntent(R.id.imgPause,getpendingIntent(this,ACTION_PAUSE));
+            remoteViews.setImageViewResource(R.id.imgPause, R.drawable.play);
+        } else {
+            remoteViews.setOnClickPendingIntent(R.id.imgPause,getpendingIntent(this,ACTION_RESUME));
+            remoteViews.setImageViewResource(R.id.imgPause, R.drawable.play);
+        }
+        remoteViews.setOnClickPendingIntent(R.id.imgPause,getpendingIntent(this,ACTION_CLEAR));
         Notification notification =
                 new Notification.Builder(this, CHANNEL_ID)
                         .setCustomContentView(remoteViews)
@@ -68,6 +127,12 @@ public class MyService extends Service {
 
 // Notification ID cannot be 0.
         startForeground(1, notification);
+    }
+    private PendingIntent getpendingIntent(@NotNull Context context, int action) {
+        Intent intent = new Intent(this, Myreceiver.class);
+        intent.putExtra("actionMusic", action);
+
+        return PendingIntent.getBroadcast(context.getApplicationContext(), action, intent, PendingIntent.FLAG_UPDATE_CURRENT);
     }
 
     @Override
@@ -84,5 +149,16 @@ public class MyService extends Service {
     public IBinder onBind(Intent intent) {
         // TODO: Return the communication channel to the service.
         throw new UnsupportedOperationException("Not yet implemented");
+    }
+    private void sendActiontoActivity(int action) {
+        Intent intent = new Intent("send_data");
+        Bundle bundle = new Bundle();
+        bundle.putSerializable("object_action", song1);
+        bundle.putBoolean("status_player", isPlaying);
+        bundle.putInt("action_music", action);
+
+        intent.putExtras(bundle);
+
+        LocalBroadcastManager.getInstance(this).sendBroadcast(intent);
     }
 }
--- week5/forestgroundServices/app/src/main/java/com/example/myapplication/Myreceiver.java
+++ week5/forestgroundServices/app/src/main/java/com/example/myapplication/Myreceiver.java
@@ -1,2 +1,18 @@
-package com.example.myapplication;public class Myreceiver {
+package com.example.myapplication;
+
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+
+public class Myreceiver extends BroadcastReceiver {
+
+    @Override
+    public void onReceive(Context context, Intent intent) {
+        int actionMusic = intent.getIntExtra("actionMusic", 0);
+
+        Intent intentservice = new Intent(context, MyService.class);
+        intentservice.putExtra("actionMusic_service",actionMusic );
+
+        context.startService(intentservice);
+    }
 }
